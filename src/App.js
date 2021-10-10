3ab37b874dcb4c953ff26d33c9c24b0d31eb4946
YousraTurk <67966028+YousraTurk@users.noreply.github.com>
2021-10-10 11:01:15 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,211 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row ">
+          <div className="col-md-12 ">
+            <nav className="navbar navbar-expand-lg navbar-light ">
+              <div className="container">
+                <a
+                  className="navbar-brand m-auto font-monospace "
+                  href="index.html"
+                >
+                  Occasions glamour
+                </a>
+                <button
+                  className="navbar-toggler"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#navbarNav"
+                  aria-controls="navbarNav"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
+                >
+                  <span className="navbar-toggler-icon"></span>
+                </button>
+                <div
+                  className="collapse navbar-collapse font-monospace"
+                  id="navbarNav"
+                >
+                  <ul className="navbar-nav p-3">
+                    <li className="nav-item">
+                      <a
+                        className="nav-link active p-2"
+                        aria-current="page"
+                        href="index.html"
+                      >
+                        <strong>Home</strong>
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a
+                        className="nav-link active p-2"
+                        aria-current="page"
+                        href="About.html"
+                      >
+                        <strong>About</strong>
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a
+                        className="nav-link active p-2"
+                        aria-current="page"
+                        href="portfolio.html"
+                      >
+                        <strong>Portfolio</strong>
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a
+                        className="nav-link active p-2"
+                        aria-current="page"
+                        href="Services.html"
+                      >
+                        <strong>Services</strong>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
+          </div>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 mx-auto">
+              <img src="./pics/favicon.png" className="img-fluid" alt="..." />
+            </div>
+          </div>
+        </div>
+        <div className="container-fluid ">
+          <div className="row  ">
+            <div className="col-md-12">
+              <img src="./pics/img6.jpg" className="img-fluid" alt="..." />
+            </div>
+          </div>
+        </div>
+        <div className="container-fluid">
+          <div className="row ">
+            <div className="col-md bg-secondary text-dark bg-light text-center p-5">
+              <h1 className=" mx-auto p-6 font-monospace">
+                <strong>Meet the party people.</strong>
+              </h1>
+            </div>
+          </div>
+        </div>
+        <div className="container-fluid mt-5">
+          <div className="row mt-5">
+            <div className="col-md-12 mt-5 mx-auto">
+              <h1 className="display-6 text-center mt-5">ABOUT US</h1>
+            </div>
+          </div>
+        </div>
+        
+
+        
+        
+        
+        <div className="container-fluid mt-5">
+            <div className="row mt-5">
+                <div className="col-md-12 mt-5">
+                    <p className="text-center"><strong> organization, details, logistics</strong> </p> <br/>
+                </div>
+            </div>
+                    <div className="row ">
+                        <div className="col-md-10 mx-auto">
+                            <p className=""> Three words that can spoil any bride or chair person’s special day. At Occasions glamour, we help
+                                manage your big ideas and your smallest concerns to ensure you are stress-free and enjoying
+                                every moment. For more than five years, we’ve been working with clients to create exceptional
+                                experiences for their wedding day, nonprofit or social gathering. As a boutique company, we’re
+                                driven by a childlike excitement for what we do and a love of collaboration with our clients.
+                                It’s our goal to leave you and your guests with an event that exceeds expectations and lasts in
+                                your memories long after the last toast.</p>
+                        </div>
+                    </div>
+                    
+
+                
+        </div>
+        <div className="container-fluid mt-5">
+            <div className="row mt-5">
+                <div className="col-md-12 mt-5">
+                    <img src="./pics/img12 (3).jpg" className="img-fluid " alt="..."/>
+                </div>
+            </div>
+        </div>
+        <div className="container-fluid bg-light mt-5">
+            <div className="row mt-5">
+                <div className="col-md-5 mt-5  me-md-5 mx-auto">
+                    <img src="./pics/img8.jpg" className="img-thumbnail" alt="..."/>
+                    <div className="row">
+                        <div className="col-md-6 mx-md-auto ">
+                            <a type="button" className="btn btn-outline-light mx-md-auto text-dark mt-5" href="services.html">SERVICES</a>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-5  mt-5 ms-md-5 mx-auto">
+                    <img src="./pics/img10.jpg" className="img-thumbnail" alt="..."/>
+                    <div className="row">
+                        <div className="col-md-6 mx-md-auto ">
+                            <a type="button" className="btn btn-outline-light mx-md-auto text-dark mt-5">OCCASIONS GlAMOUR</a>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            
+          </div>
+          <div className="container">
+              <div className="row">
+                  <div className="col-md-4 offset-md-4">
+                    <button type="button" className="btn btn-outline-light">Light</button>
+                  </div>
+              </div>
+          </div> 
+    
+    <section id="footer" className="p-5 mt-5">
+        <div className="container">
+            <div className="row text-center text-xs-center text-sm-left text-md-left">
+                <div className="col-xs-12 col-sm-4 col-md-4">
+                    <h5>Quick links</h5>
+                    <ul className="list-unstyled quick-links">
+                        <li><a href="index.html"><i className="fa fa-angle-double-right"></i>OCCASIONS GlAMOUR</a></li>
+                        <li><a href="index.html"><i className="fa fa-angle-double-right"></i>HOME</a></li>
+                        <li><a href="About.html"><i className="fa fa-angle-double-right"></i>ABOUT</a></li>
+                        <li><a href="Services.html"><i className="fa fa-angle-double-right"></i>SERVICES</a></li>
+                        <li><a href="portfolio.html"><i className="fa fa-angle-double-right"></i>PORTFOLIO</a></li>
+                    </ul>
+                </div>
+                <div className="col-xs-12 col-sm-4 col-md-4">
+                    <h5>Quick links</h5>
+                    <ul className="list-unstyled quick-links">
+                        <li><a href="index.html"><i className="fa fa-angle-double-right"></i>OCCASIONS GlAMOUR</a></li>
+                        <li><a href="index.html"><i className="fa fa-angle-double-right"></i>HOME</a></li>
+                        <li><a href="About.html"><i className="fa fa-angle-double-right"></i>ABOUT</a></li>
+                        <li><a href="Services.html"><i className="fa fa-angle-double-right"></i>SERVICES</a></li>
+                        <li><a href="portfolio.html"><i className="fa fa-angle-double-right"></i>PORTFOLIO</a></li>
+                    </ul>
+                </div>
+                <div className="col-xs-12 col-sm-4 col-md-4">
+                    <h5>Quick links</h5>
+                    <ul className="list-unstyled quick-links">
+                        <li><a href="index.html"><i className="fa fa-angle-double-right"></i>OCCASIONS GlAMOUR</a></li>
+                        <li><a href="index.html"><i className="fa fa-angle-double-right"></i>HOME</a></li>
+                        <li><a href="About.html"><i className="fa fa-angle-double-right"></i>ABOUT</a></li>
+                        <li><a href="Services.html"><i className="fa fa-angle-double-right"></i>SERVICES</a></li>
+                        <li><a href="portfolio.html"><i className="fa fa-angle-double-right"></i>PORTFOLIO</a></li>
+                    </ul>
+                </div>
+            </div>
+            </div>
+            </section>
+
+      </div>
+    );
+  }
+}
+export default App;
